4714fc65e801ba6fbe3b5be21369ef496cbde4e2
carlos solis p <solis.krlos@gmail.com>
2022-04-07 16:35:23 -0500

    c2

--- copiar.ps1
+++ copiar.ps1
@@ -4,25 +4,31 @@ C:\xampp\htdocs\restaurante\...  ->  F:\xampp\htdocs\restaurante\...
 #>
 
 #Clear-Host;
-$archivos = Get-ChildItem -Path c:\xampp\htdocs\restaurante -Recurse | Where-Object{$_.LastWriteTime -gt (Get-Date).AddHours(-24)};
-
-foreach($item in $archivos)
+$v  = Get-Volume
+foreach($i in $v.driveletter)
 {
-    $file = "$($item.Directory)\$($item.name)"; 
-    if(Test-Path -Path $file -PathType Leaf)
-    {
-        $arc  = "F" + $file.Substring(1, $file.Length - 1);
-        try 
+    if($i -eq "F")
+    {        
+        $archivos = Get-ChildItem -Path . -Recurse | Where-Object{$_.LastWriteTime -gt (Get-Date).AddHours(-24)}
+
+        foreach($item in $archivos)
         {
-            # Copy-Item $file $arc -Confirm:$False; 
-            $arc
-            
+            $file = "$($item.Directory)\$($item.name)"; 
+            if(Test-Path -Path $file -PathType Leaf)
+            {
+                $arc  = "F" + $file.Substring(1, $file.Length - 1);
+                try 
+                {
+                    $arc
+                    Copy-Item $file $arc -Confirm:$False 
+                    
+                }
+                catch 
+                {
+                    Write-Host "Error: " $file -ForegroundColor Yellow; 
+                }
+            }
         }
-        catch 
-        {
-            Write-Host "Error: " $file -ForegroundColor Yellow; 
-        }
-        # Write-Host $arc ; 
-        # Write-Host $file; 
+        break
     }
 }
